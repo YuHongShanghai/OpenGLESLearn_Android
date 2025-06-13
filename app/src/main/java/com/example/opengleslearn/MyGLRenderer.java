4f5ae7e68861b252df5d2d2837fae81418446211
YuHong <yh951012@163.com>
2025-06-13 12:51:21 +0800

    feat: add motion

--- app/src/main/java/com/example/opengleslearn/MyGLRenderer.java
+++ app/src/main/java/com/example/opengleslearn/MyGLRenderer.java
@@ -6,6 +6,7 @@ import javax.microedition.khronos.opengles.GL10;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
     private Triangle mTriangle;
@@ -14,6 +15,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private final float[] vPMatrix = new float[16];
     private final float[] projectionMatrix = new float[16];
     private final float[] viewMatrix = new float[16];
+    private float[] rotationMatrix = new float[16];
 
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         // Set the background frame color
@@ -25,6 +27,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onDrawFrame(GL10 unused) {
+        float[] scratch = new float[16];
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
         // Set the camera position (View matrix)
@@ -33,8 +36,18 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
 
+        // Create a rotation transformation for the triangle
+        long time = SystemClock.uptimeMillis() % 4000L;
+        float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(rotationMatrix, 0, angle, 0, 0, -1.0f);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the vPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0);
+
         // Draw shape
-        mSquare.draw(vPMatrix);
+        mSquare.draw(scratch);
     }
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
